02a0a50ba616256309d3d1e796b94ca7f05b9d62
Carlos Henrique <carlinhos_nino10@hotmail.com>
2021-05-04 17:40:36 -0300

    cadastro e validação

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const { request, response } = require("express");
+const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+app.use(express.json());
+
+const customers = [];
+
+app.post("/account", (request, response) => {
+    const { cpf, name } = request.body;
+
+    const customerAlreadyExists = customers.some(
+        (custumer) => custumer.cpf === cpf);
+
+    if ( customerAlreadyExists) {
+        return response.status(400).json({ error: "Custumer alredy exist!"});
+    }
+    
+
+    customers.push({
+        cpf,
+        name,
+        id: uuidv4(),
+        statement: []
+    });
+
+    return response.status(201).send();
+
+});
+
+app.get("/statement", (request, response) => {
+const { cpf } = request.headers;
+
+const customer = customers.find((customer) => customer.cpf === cpf);
+
+if(!customer) {
+    return response.status(400).json({ error: "Customer not found!"});
+};
+
+return response.json(customer.statement);
+});
+
+app.listen(3333);
